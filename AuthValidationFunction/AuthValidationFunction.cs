cf014d66783e234d486f1adb7d4780cab3a466e3
Rajeev Maurya <rajeevmaurya@Rajeevs-MacBook-Air.local>
2023-05-26 13:01:51 +0530

    Added files

---
+++ AuthValidationFunction/AuthValidationFunction.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+
+namespace AuthValidationFunction
+{
+    public class AuthValidationFunction
+    {
+        private readonly ILogger _logger;
+
+        public AuthValidationFunction(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<AuthValidationFunction>();
+        }
+
+        [Function("AuthValidationFunction")]
+        public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+
+            var response = req.CreateResponse(HttpStatusCode.OK);
+            response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+
+            response.WriteString("Welcome to Azure Functions!");
+
+            return response;
+        }
+    }
+}
+
